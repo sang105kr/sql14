f6e646e231a6f8d574744b51c04e1d0c82368adc
sanghak yi <sang105kr@gmail.com>
2025-04-18 15:50:49 +0900

    20250418-6

--- day2/day2_연습문제.sql
+++ day2/day2_연습문제.sql
@@ -30,14 +30,54 @@
 	                   AND t3.bookid = t1.bookid); 
 
 --(6) 박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이
-
+	SELECT t3.bookname "이름", t3.price "가격", t3.price - t2.saleprice "정가와 판매가격의 차이"
+	  FROM customer t1 INNER JOIN	orders t2 On t1.custid = t2.custid
+	                   INNER JOIN	book t3   ON t2.bookid = t3.bookid
+	 WHERE t1.name = '박지성'; 
 
 --(7) 박지성이 구매하지 않은 도서의 이름
+--case1) 차집합
+   SELECT bookname
+     FROM book
+	 MINUS
+	SELECT t3.bookname
+	  FROM customer t1, orders t2, book t3
+	 WHERE t1.custid = t2.custid
+	   AND t2.bookid = t3.bookid
+     AND t1.name = '박지성'; 
 
-
+--case2) sub-query 비상관쿼리 not in
+	SELECT bookname
+	  FROM book
+	 WHERE bookid NOT IN ( SELECT t1.bookid
+	                         FROM orders t1 INNER JOIN customer t2 ON t1.custid = t2.CUSTID 
+	                        WHERE t2.name = '박지성' );
+--case3) sub-query 상관쿼리 not exists
+	SELECT bookname
+	  FROM book t3
+	 WHERE NOT EXISTS	( SELECT *
+                        FROM orders t1 INNER JOIN customer t2 ON t1.custid = t2.CUSTID 
+                       WHERE t2.name = '박지성'
+                         AND t3.bookid = t1.bookid);
+--case4) left outer join
+	SELECT DISTINCT t1.bookname
+	  FROM BOOK t1 LEFT OUTER JOIN orders t2   ON t1.bookid = t2.bookid
+                 LEFT OUTER JOIN customer t3 ON t2.custid = t3.CUSTID
+   WHERE t3.name != '박지성' OR t3.name IS null;
+	 
 --2. 마당서점의 운영자와 경영자가 요구하는 다음 질문에 대해 SQL 문을 작성하시오.
 --(8) 주문하지 않은 고객의 이름(부속질의 사용)
-
+--case1) 비상관 쿼리
+	SELECT name
+	  FROM customer
+	 WHERE custid NOT IN  ( SELECT custid
+	                          FROM ORDERS );
+--case2) 상관쿼리
+	SELECT name
+	  FROM customer t1
+	 WHERE NOT exists  ( SELECT *
+	                       FROM ORDERS t2
+											  WHERE t1.custid = t2.custid );
 
 --(9) 주문 금액의 총액과 주문의 평균 금액
 
